8f6d762187fcd1924e89411e1871e5844ac6f437
Shiven Bansal <shivenapj@gmail.com>
2024-10-07 14:31:00 +0530

    minor changes

--- exp5/part 3/primeNos.js
+++ exp5/part 3/primeNos.js
@@ -1,27 +1,22 @@
 function* generatePrimes(limit) {
+    // Simplified version of the isPrime function
     function isPrime(num) {
         if (num <= 1) return false;
-        if (num <= 3) return true;
-        if (num % 2 === 0 || num % 3 === 0) return false;
-        let i = 5;
-        while (i * i <= num) {
-            if (num % i === 0 || num % (i + 2) === 0) return false;
-            i += 6;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
         }
         return true;
     }
 
     for (let num = 2; num <= limit; num++) {
-        if (isPrime(num)) {
-            yield num;
-        }
+        if (isPrime(num)) yield num;
     }
 }
 
 function getLimitFromCommandLine() {
     const args = process.argv.slice(2);
     const limit = Number(args[0]);
-    
+
     if (isNaN(limit) || limit <= 1) {
         console.error('Error: Please provide a valid limit greater than 1.');
         process.exit(1);
@@ -32,10 +27,10 @@ function getLimitFromCommandLine() {
 
 function main() {
     const limit = getLimitFromCommandLine();
-    const primeGenerator = generatePrimes(limit);
-
     console.log(`Prime numbers up to ${limit}:`);
-    for (const prime of primeGenerator) {
+    
+    // Using generator to print prime numbers
+    for (const prime of generatePrimes(limit)) {
         console.log(prime);
     }
 }
